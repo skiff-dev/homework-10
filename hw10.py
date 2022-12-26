0e2a074daa0eec04111c6ba6621dc6b717098de8
dunkin.v <dunkin.v@ajax.systems>
2022-12-26 14:00:08 +0200

    by Skiff

--- hw10.py
+++ hw10.py
@@ -2,39 +2,57 @@ from collections import UserDict
 
 
 class Field:
-    pass
+    def __init__(self, value):
+        self.value = value
+    
+    def __repr__(self) -> str:
+        return self.value
 
 
 class Name(Field):
-    def __init__(self, name):
-        self.value = name
+    pass
 
 
 class Phone(Field):
-    def __init__(self, phone):
-        self.value = phone
+    pass
 
 
 class Record:
-    def __init__(self, name, phone=None):
+    def __init__(self, name: Name, phone: Phone=None):
         self.name = name
         self.phones = []
         if phone:
             self.phones.append(phone)
 
-    def add_phone(self, phone):
-        new_phone = ''
-        for i in filter(lambda x: x.isnumeric(), phone):
-            new_phone += i
-        self.phones.append(new_phone)
+    def add_phone(self, phone: Phone):
+        if isinstance(phone, Phone):
+            self.phones.append(phone)
+            return f'{phone.value} add success to contact {self.name.value}'
+        return f'Sorry, phone must be a Phone instance'
 
     def delete_phone(self, phone):
         self.phones.remove(phone)
 
-    def edit_phone(self, old_phone, new_phone):
+    def edit_phone(self, old_phone: Phone, new_phone: Phone):
         self.phones[self.phones.index(old_phone)] = new_phone
 
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
+
+
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('123457')
+    rec = Record(name, phone)
+    phone2 = Phone('0987654')
+    print(rec.add_phone(phone2))
+    phone3 = '555555555'
+    print(rec.add_phone(phone3))
+
+    print(rec.phones)
+
+    rec.edit_phone(phone2, Phone('6788989'))
+
+    print(rec.phones)
